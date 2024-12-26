8d7c64abc22df3a6b0badb156bd068d9eefb45dc
OhHyukJun <kmh7277@naver.com>
2024-12-26 14:11:35 +0900

    Fix: 지도 겹치는 버그 수정정

--- mini7/src/main/resources/static/js/drawMaps.js
+++ mini7/src/main/resources/static/js/drawMaps.js
@@ -1,8 +1,27 @@
-function drawRoute(startLat, startLng, endLat, endLng) {
-    var map = new naver.maps.Map('map', {
+let map;
+let polyline; 
+const markers = []; 
+
+function initializeMap(startLat, startLng, endLat, endLng) {
+    map = new naver.maps.Map('map', {
         center: new naver.maps.LatLng((startLat + endLat) / 2, (startLng + endLng) / 2),
         zoom: 10
     });
+}
+
+function clearRoute() {
+    if (polyline) {
+        polyline.setMap(null);
+    }
+    markers.forEach(marker => marker.setMap(null));
+    markers.length = 0;
+
+function drawRoute(startLat, startLng, endLat, endLng) {
+    clearRoute();
+
+    if (!map) {
+        initializeMap(startLat, startLng, endLat, endLng);
+    }
 
     fetch(`/api/map/rest/dynamic?startLat=${startLat}&startLng=${startLng}&endLat=${endLat}&endLng=${endLng}`)
         .then(response => {
@@ -14,14 +33,14 @@ function drawRoute(startLat, startLng, endLat, endLng) {
         .then(data => {
             if (data.code === 0 && data.route && data.route.traoptimal) {
                 const path = data.route.traoptimal[0].path;
-                const polyline = new naver.maps.Polyline({
+                polyline = new naver.maps.Polyline({
                     path: path.map(coord => new naver.maps.LatLng(coord[1], coord[0])),
                     strokeColor: '#5347AA',
                     strokeWeight: 2,
                     map: map
                 });
 
-                new naver.maps.Marker({
+                const startMarker = new naver.maps.Marker({
                     position: new naver.maps.LatLng(startLat, startLng),
                     map: map,
                     icon: {
@@ -31,8 +50,9 @@ function drawRoute(startLat, startLng, endLat, endLng) {
                         anchor: new naver.maps.Point(16, 16)
                     }
                 });
+                markers.push(startMarker);
 
-                new naver.maps.Marker({
+                const endMarker = new naver.maps.Marker({
                     position: new naver.maps.LatLng(endLat, endLng),
                     map: map,
                     icon: {
@@ -42,6 +62,7 @@ function drawRoute(startLat, startLng, endLat, endLng) {
                         anchor: new naver.maps.Point(16, 16)
                     }
                 });
+                markers.push(endMarker);
 
                 const bounds = polyline.getBounds();
                 map.fitBounds(bounds);
@@ -51,61 +72,3 @@ function drawRoute(startLat, startLng, endLat, endLng) {
         })
         .catch(error => console.error('Error:', error));
 }
-
-
-/*const urlParams = new URLSearchParams(window.location.search);
-const startLat = parseFloat(urlParams.get('startLat'));
-const startLng = parseFloat(urlParams.get('startLng'));
-const endLat = parseFloat(urlParams.get('endLat'));
-const endLng = parseFloat(urlParams.get('endLng'));
-
-var map = new naver.maps.Map('map', {
-        center: new naver.maps.LatLng((startLat + endLat) / 2, (startLng + endLng) / 2),
-        zoom: 10
-});
-
-function drawRoute(startLat, startLng, endLat, endLng) {
-    fetch(`/api/map/rest/dynamic?startLat=${startLat}&startLng=${startLng}&endLat=${endLat}&endLng=${endLng}`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.code === 0 && data.route && data.route.traoptimal) {
-                const path = data.route.traoptimal[0].path;
-                const polyline = new naver.maps.Polyline({
-                    path: path.map(coord => new naver.maps.LatLng(coord[1], coord[0])),
-                    strokeColor: '#5347AA',
-                    strokeWeight: 2,
-                    map: map
-                });
-
-                var startMarker = new naver.maps.Marker({
-                    position: new naver.maps.LatLng(startLat, startLng),
-                    map: map,
-                    icon: {
-                        url: '/image/patient.png',
-                        scaledSize: new naver.maps.Size(50, 50),
-                        origin: new naver.maps.Point(0, 0),
-                        anchor: new naver.maps.Point(16, 16)
-                    }
-                });
-
-                var endMarker = new naver.maps.Marker({
-                    position: new naver.maps.LatLng(endLat, endLng),
-                    map: map,
-                    icon: {
-                        url: '/image/hospital.png',
-                        scaledSize: new naver.maps.Size(50, 50),
-                        origin: new naver.maps.Point(0, 0),
-                        anchor: new naver.maps.Point(16, 16)
-                    }
-                });
-
-                const bounds = polyline.getBounds();
-                map.fitBounds(bounds);
-            } else {
-                console.error("Failed to get route data");
-            }
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-drawRoute(startLat, startLng, endLat, endLng);*/
