b03bd2d00c7e4ae38785cd6d15b7ec76e070da78
OhHyukJun <kmh7277@naver.com>
2024-12-26 11:55:07 +0900

    Feat: 지도 모달 구현

--- mini7/src/main/resources/static/js/drawMaps.js
+++ mini7/src/main/resources/static/js/drawMaps.js
@@ -1,17 +1,18 @@
-const urlParams = new URLSearchParams(window.location.search);
-const startLat = parseFloat(urlParams.get('startLat'));
-const startLng = parseFloat(urlParams.get('startLng'));
-const endLat = parseFloat(urlParams.get('endLat'));
-const endLng = parseFloat(urlParams.get('endLng'));
-
-var map = new naver.maps.Map('map', {
-        center: new naver.maps.LatLng((startLat + endLat) / 2, (startLng + endLng) / 2),
-        zoom: 10
-});
 
 function drawRoute(startLat, startLng, endLat, endLng) {
+
+    var map = new naver.maps.Map('map', {
+        center: new naver.maps.LatLng((startLat + endLat) / 2, (startLng + endLng) / 2),
+        zoom: 10
+    });
+
     fetch(`/api/map/rest/dynamic?startLat=${startLat}&startLng=${startLng}&endLat=${endLat}&endLng=${endLng}`)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
             if (data.code === 0 && data.route && data.route.traoptimal) {
                 const path = data.route.traoptimal[0].path;
@@ -22,7 +23,7 @@ function drawRoute(startLat, startLng, endLat, endLng) {
                     map: map
                 });
 
-                var startMarker = new naver.maps.Marker({
+                new naver.maps.Marker({
                     position: new naver.maps.LatLng(startLat, startLng),
                     map: map,
                     icon: {
@@ -33,7 +34,7 @@ function drawRoute(startLat, startLng, endLat, endLng) {
                     }
                 });
 
-                var endMarker = new naver.maps.Marker({
+                new naver.maps.Marker({
                     position: new naver.maps.LatLng(endLat, endLng),
                     map: map,
                     icon: {
@@ -52,5 +53,3 @@ function drawRoute(startLat, startLng, endLat, endLng) {
         })
         .catch(error => console.error('Error:', error));
 }
-
-drawRoute(startLat, startLng, endLat, endLng);
